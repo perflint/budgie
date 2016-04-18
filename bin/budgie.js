0a4e864fb0a226d031f11fd56cb9e949a652e250
Matthew Harrison-Jones <matthojo@gmail.com>
2016-04-18 10:10:46 +0100

    Initial Questions setup

---
+++ bin/budgie.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var debug = (process.argv.indexOf('--debug') > -1)
+
+// must do this initialization *before* other requires in order to work
+if (debug) {
+  require('debug').enable('budgie:*')
+}
+
+var cli = require('../lib/cli')
+
+cli.interpret(process.argv)
